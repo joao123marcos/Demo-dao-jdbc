f39e268b453bf9d8faad621eeec30d4fd1c9b781
“joaomarcos” <Joaomarcos.spires@gmail.com>
2025-05-14 11:38:30 -0300

    Classe departamento

---
+++ src/main/Program.java
@@ -0,0 +1,11 @@
+package main;
+
+import Model.Class.Department;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
+}
